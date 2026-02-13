c86ff75017af654d0355e6cb68f1d870b7f2b913
Tejadorasetti <thedevops.learnerxgmail.com>
2026-02-13 07:26:37 +0530

    shipping

--- shipping.sh
+++ shipping.sh
@@ -78,10 +78,18 @@ VALIDATE $? "copying shipping systemd service file"
 dnf install mysql -y &>>$LOGS_FILE
 VALIDATE $? "installing mysql client"
 
+mysql -h $MYSQL_HOST -uroot -pRoboShop@1 -e 'use cities'
+
+if [ $? -ne 0 ]; then
 mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/schema.sql &>>$LOGS_FILE
 mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/app-user.sql &>>$LOGS_FILE
 mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/master-data.sql &>>$LOGS_FILE
-VALIDATE $? "loading shipping schema and data"
+VALIDATE $? "loaded shipping schema and data"
+else
+    echo -e "$Y shipping database already exists, skipping database creation $N" &>>$LOGS_FILE
+fi
+
+
 
 systemctl enable shipping &>>$LOGS_FILE
 VALIDATE $? "enabling shipping service"
